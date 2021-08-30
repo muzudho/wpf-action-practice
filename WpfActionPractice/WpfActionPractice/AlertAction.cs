015f31f203139159973cdace53770012a1a4a5d0
むずでょ <muzudho1@gmail.com>
2021-08-30 22:26:01 +0900

    AlertAction

---
+++ WpfActionPractice/WpfActionPractice/AlertAction.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfActionPractice
+{
+    /// <summary>
+    /// ボタンを押したら、警告ダイアログボックスが出るということ
+    /// </summary>
+    public class AlertAction : TriggerAction<Button>
+    {
+        /// <summary>
+        /// ダイアログボックスに表示する任意の文字列です
+        /// </summary>
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(AlertAction), new UIPropertyMetadata(null));
+
+        public AlertAction()
+        {
+        }
+
+        /// <summary>
+        /// メッセージが入力されていたらメッセージボックスを出す
+        /// </summary>
+        /// <param name="o"></param>
+        protected override void Invoke(object o)
+        {
+            if (string.IsNullOrEmpty(this.Message))
+            {
+                return;
+            }
+
+            MessageBox.Show(this.Message);
+        }
+    }
+}
