be6db8bc0b9969d47ccc8e80ea18ce0679740b4b
むずでょ <muzudho1@gmail.com>
2021-09-17 20:00:19 +0900

    MainWindow の名前の末尾に View 付けた

---
+++ WpfActionPractice/WpfActionPractice/MainWindowView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace WpfActionPractice
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
